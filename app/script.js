f24101e18f5a5a4aa7378aa2bfdd3610899eb15f
PaulCollins99 <paul.collins@node-it.com>
2020-07-27 23:15:53 +0100

    navbar done

--- app/script.js
+++ app/script.js
@@ -11,8 +11,28 @@ if (navigator.serviceWorker) {
     window.addEventListener('load', registerServiceWorker);
 }
 
+//document.querySelectorAll('[data-toggle-navbar]').forEach(toggle => {
+//    toggle.addEventListener('click', e => {
+//      const navID = toggle.dataset.toggleNavbar;
+//      const navElement = navID ? document.getElementById(navID) : undefined;
+//      console.log(navElement)
+//      if (navElement) {
+//         let navState = navElement.getAttribute('aria-hidden');
+//         navElement.setAttribute('aria-hidden', navState == 'true' ? false : true); 
+//      }
+//    });
+// });
+
+function toggle () {
+    const navElement = document.getElementById("navbar")
+    let navState = navElement.getAttribute('aria-hidden');
+    navElement.setAttribute('aria-hidden', navState == 'true' ? false : true); 
+}
+
 function boot () {
-    
+    window.openNav.addEventListener('click', toggle);
+    window.closeNav.addEventListener('click', toggle);
 }
 
 document.addEventListener('load', boot);
+boot();
