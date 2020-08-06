ce1ee626fdc58ec69c05889ab7eedf716db48217
PaulCollins99 <paul.collins@node-it.com>
2020-08-06 14:14:39 +0100

    test2

--- app/editNotes.js
+++ app/editNotes.js
@@ -88,8 +88,8 @@ function boot () {
 }
 
 setTimeout(function () {
-    let viewheight = $(window).height();
-    let viewwidth = $(window).width();
+    let viewheight = window.outerHeight
+    let viewwidth = window.outerWidth
     let viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
 }, 300);
